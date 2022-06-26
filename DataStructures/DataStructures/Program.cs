87d9ca9c38d680d5e8320d30f2a22ae171b916d3
akshayb407 <akshayb407@gmail.com>
2022-06-26 13:29:45 +0530

    UC3

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -6,16 +6,13 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Stacks Method");
+            Console.WriteLine("Queue Method");
 
-            LinkedListStack Stack = new LinkedListStack();
-            Stack.Push(70);
-            Stack.Push(30);
-            Stack.Push(56);
-            Stack.Display();
-            Stack.Pop();
-            Stack.Peek();
-            Stack.IsEmpty();
+            LinkedListQueue Queue = new LinkedListQueue();
+            Queue.Enqueue(56);
+            Queue.Enqueue(30);
+            Queue.Enqueue(70);
+            Queue.Display();
         }
     }
 }
